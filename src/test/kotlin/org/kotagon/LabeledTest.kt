88920c93115013099a575834fb72cbfb35295a53
Vladislav Lamzenkov <mcflydesigner@gmail.com>
2025-04-23 02:11:13 +0300

    feature: added more tests to LabeledTest.kt

--- src/test/kotlin/org/kotagon/LabeledTest.kt
+++ src/test/kotlin/org/kotagon/LabeledTest.kt
@@ -1,18 +1,64 @@
 package org.kotagon
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
 import kotlin.test.Test
 
-object AnyString : Policy({
+object AnyStringPolicy : Policy({
     any<String>()
+})
+
+object ConcreteStringPolicy : Policy({
+    "ConcreteTestString"
 })
 
 class LabeledTest {
 
     @Test
-    fun testAllowedToFlowSamePolicy() {
+    @DisplayName("The information flow is allowed to the same policy (same object)")
+    fun testAllowedToFlowSamePolicy1() {
         withPolicyEvaluationContext {
-            assertTrue(allowedToFlow(AnyString, AnyString))
+            assertTrue(allowedToFlow(AnyStringPolicy, AnyStringPolicy))
+        }
+    }
+
+    @Test
+    @DisplayName("The information flow is allowed to the same policy (different objects)")
+    fun testAllowedToFlowSamePolicy2() {
+        val anotherAnyStringPolicy = object : Policy({
+            any<String>()
+        }) {}
+
+        withPolicyEvaluationContext {
+            assertTrue(allowedToFlow(AnyStringPolicy, anotherAnyStringPolicy))
+        }
+    }
+
+    @Test
+    @DisplayName("The information flow is allowed to the same policy (different objects)")
+    fun testAllowedToFlowSamePolicy3() {
+        val anotherAnyStringPolicy = object : Policy({
+            any(String::class)
+        }) {}
+
+        withPolicyEvaluationContext {
+            assertTrue(allowedToFlow(AnyStringPolicy, anotherAnyStringPolicy))
+        }
+    }
+
+    @Test
+    @DisplayName("The information flow is allowed to more strict policy")
+    fun testAllowedToFlowStrictPolicy() {
+        withPolicyEvaluationContext {
+            assertTrue(allowedToFlow(AnyStringPolicy, ConcreteStringPolicy))
+        }
+    }
+
+    @Test
+    @DisplayName("The information flow is not allowed to more general policy")
+    fun testNotAllowedToFlowGeneralPolicy() {
+        withPolicyEvaluationContext {
+            assertFalse(allowedToFlow(ConcreteStringPolicy, AnyStringPolicy))
         }
     }
 }
